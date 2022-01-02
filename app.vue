facfaa757da5f10fbddaaf00bd0e857fa3a4f982
root <andreas@moving-bytes.at>
2022-01-02 11:56:15 +0100

    inital commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+<script  lang="ts">
+import { LineChart } from 'vue-chart-3';
+export default {
+    name: "report",
+    components: { LineChart },
+
+}
+</script>
+
